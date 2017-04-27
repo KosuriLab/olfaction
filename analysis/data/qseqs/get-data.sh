362a7aa2616e266aa09386751aa622b01005dd7c
nlubock <nlubock@ucla.edu>
2017-04-26 19:49:21 -0700

    Adding second pilot run

--- analysis/data/qseqs/get-data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-for f in ../../../raw-data/032217_Olfaction_Pilot/*qseq.txt.gz; do ln -s $f $(basename $f); done
-# rename -v 's/s_1_([1-2])_([0-9]*).*/$2_$1.qseq.gz/' qseqs/*.gz > rename.err
